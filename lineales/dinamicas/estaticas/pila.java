3d6edae8c7a2385dff817c41975e3944addf4d3c
jako <jakothedev@gmail.com>
2025-09-14 18:30:55 -0300

    paso1

--- lineales/dinamicas/estaticas/pila.java
+++ lineales/dinamicas/estaticas/pila.java
@@ -2,8 +2,28 @@ package lineales.dinamicas.estaticas;
 
 class pila {
 
-    int tope = -1;
-
+    private int tope;
+    private Object [] arreglo;
+    private static final int TAMANIO = 20;
     
+    public pila(){
+        this.arreglo = new Object[TAMANIO];
+        this.tope = -1;
+    }
     
+    public boolean apilar(Object newElement){
+        return privateApilar(newElement);
+    }
+    // private mode
+    private boolean privateApilar(Object elem){
+        boolean exito;
+        if (this.tope+1 >= this.TAMANIO){
+            exito = false;
+        }else{
+            this.tope++;
+            this.arreglo[tope] = elem;
+            exito = true;
+        }
+        return exito;
+    }
 }
