b0ccebccc97593b7ae2e62be3f3fec7ea0459c5e
jako <jakothedev@gmail.com>
2025-09-14 22:26:22 -0300

    paso1completo

--- lineales/dinamicas/estaticas/pila.java
+++ lineales/dinamicas/estaticas/pila.java
@@ -6,15 +6,18 @@ class pila {
     private Object [] arreglo;
     private static final int TAMANIO = 20;
     
+    //Constructor
     public pila(){
         this.arreglo = new Object[TAMANIO];
         this.tope = -1;
     }
-    
+
+    // public module
     public boolean apilar(Object newElement){
         return privateApilar(newElement);
     }
-    // private mode
+
+    // private module
     private boolean privateApilar(Object elem){
         boolean exito;
         if (this.tope+1 >= this.TAMANIO){
@@ -26,4 +29,51 @@ class pila {
         }
         return exito;
     }
-}
+
+    //public module 
+    public boolean desapilar(){
+        return privateDesapilar();
+    }
+
+    //
+    private boolean privateDesapilar(){
+        boolean exito = false;
+
+        if (!esVacia()) {
+            arreglo[tope] = null;
+            tope--;
+            exito = true;    
+        }
+        return exito;
+    }
+
+    public boolean esVacia(){
+        return tope == -1;
+    }
+
+    // metodo vaciar
+    public void vaciar(){
+        if (!esVacia()) {
+            while (tope>-1) {
+                arreglo[tope] = null;
+                tope--;
+            }
+        }
+    }
+
+    // OBTENER TOPE
+    public Object obtenerTope(){
+        return privateObtenerTope();
+        
+    }
+    
+    private Object privateObtenerTope(){
+        Object elTopeActual = null;
+
+        if (!esVacia()) {
+            elTopeActual = arreglo[tope];
+            }
+            return elTopeActual;
+
+        }
+    }
